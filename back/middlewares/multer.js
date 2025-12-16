63a0792ab92a78a621add5c44d494b38e57975b1
gonza laptop <gonz.r@hotmail.com>
2025-12-16 14:10:23 -0300

    se anade middleware cloudinary  para la suba de imagenes

--- back/middlewares/multer.js
+++ back/middlewares/multer.js
@@ -1,4 +1,29 @@
 import multer from "multer";
-const upload = multer({ dest: "uploads/" });
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        secure: true,
+        folder: "mi-tienda-productos", //nombre de la carpeta en cloudinary
+        allowed_formats: ["jpg", "png", "jpeg", "webp"], //formatos permitidos
+        transformation: [{ width: 500, height: 500, crop: "limit" }] //opcional: redimension al subir
+    }
+});
+
+
+
+const upload = multer({ storage: storage });
+//const upload = multer({ dest: "uploads/" }) old way local storage
 
 export default upload;
